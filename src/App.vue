cd712112ad7a5af65003e18e8b344ba31f59c15c
Falon Darville <falondarville@gmail.com>
2018-05-20 13:29:27 -0700

    add test file and component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <test></test>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Test from './components/test'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Test
+  }
 }
 </script>
 
---
+++ src/components/test.vue
@@ -0,0 +1,19 @@
+/* eslint-disable */
+<template>
+	<div class='test'>
+
+		<h1>Test</h1>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Test'
+	}
+
+</script>
+
+<style scoped>
+
+</style>
