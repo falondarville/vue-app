c0f840369f71af53debcf0d2321bbb8158f6a57b
Falon Darville <falondarville@gmail.com>
2018-05-20 17:52:22 -0700

    add vue router

--- src/main.js
+++ src/main.js
@@ -2,17 +2,40 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import Router from './router'
 import vueResource from 'vue-resource'
+import vueRouter from 'vue-router'
+import Users from './components/Users'
+import Test from './components/test'
 
 Vue.use(vueResource)
+Vue.use(vueRouter)
+
+const router = new vueRouter({
+	mode: 'history',
+	base: __dirname,
+	routes: [
+		{path: '/', component: Users},
+		{path: '/test', component: Test}
+	]
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  template: `
+  <div id='app'>
+  	<ul>
+		<li>
+			<router-link to='/'>Users</router-link>
+		</li>
+		<li>
+			<router-link to='/test'>Test</router-link>
+		</li>
+  	</ul>
+  	<router-view></router-view>
+  	</div>
+  	`
+}).$mount(`#app`)
